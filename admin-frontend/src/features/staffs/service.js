dccb2f273ea670c7d4b99989d44a4eafc88f68e1
Rukshan Perera <rukshansuranga@outlook.com>
2023-02-18 07:30:59 +0530

    change url setting

--- admin-frontend/src/features/staffs/service.js
+++ admin-frontend/src/features/staffs/service.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-type": "application/json"
   }
